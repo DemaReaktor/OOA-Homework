178575b72c3b00bd253ea30c965099b5a633e88b
Dema <67798322+DemaReaktor@users.noreply.github.com>
2021-09-15 23:21:54 +0300

    Task 1 and 3 was updated by fonded new methods and decreased codes (Task 1: used method eval()) (Task 2: used methods eval(),split() and replace(), removed module operator)

--- Task1.py
+++ Task1.py
@@ -1,3 +1,2 @@
-from operator import *
 from sys import argv
-print({'+': add, '-': sub, '*': mul, '/': truediv}[argv[2]](int(argv[1]), int(argv[3])))
+print(eval(argv[1]+argv[2]+argv[3]))
--- Task3.py
+++ Task3.py
@@ -1,22 +1,11 @@
-from _operator import add, sub
 from sys import argv
 
 
-def formula(text, number=""):
-    """method convert text into expression and return value"""
-    if text.isdigit():
-        return int(text)
-    if (text[0] == '+' or text[0] == '-') and number and text[1:]:
-        return sign(text[0], number, text[1:len(text)])
-    if text[0].isdigit():
-        return formula(text[1:len(text)], number+text[0])
+def isExpression(text):
+    for number in text.replace('-', '+').split('+'):
+        if not number.isdigit():
+            return 0
+    return text[0].isdigit() and text[-1].isdigit()
 
 
-def sign(operator, text1, text2):
-    """method do operation(sum or subtract) with values of texts"""
-    if formula(text2):
-        return {'+': add, '-': sub}[operator](formula(text1), formula(text2))
-
-
-result = formula(argv[1])
-print("(True,{0})".format(result) if result else "(False,None)")
+print("(True,{0})".format(eval(argv[1])) if isExpression(argv[1]) else "(False,None)")
