52090b8f035a75f950b6e3c409f8b8a059d90193
Dema <67798322+DemaReaktor@users.noreply.github.com>
2021-10-20 19:58:06 +0300

    Have done tasks with all requirements.

--- Task1.py
+++ Task1.py
@@ -1,26 +1,26 @@
 class Rectangle:
+    """a figure"""
     def __init__(self, width=1., length=1.):
-        self.__width = (width if width < 20. else 20.) if width > 0. else 0.
-        self.__length = (length if length < 20. else 20.) if length > 0. else 0.
+        self.__width = width
+        self.__length = length
 
-    def perimetr(self):
+    def perimeter(self):
+        """return the perimeter"""
         return 2.*(self.__width+self.__length)
 
     def square(self):
+        """return the square"""
         return self.__width*self.__length
 
-    def get_width(self):
-        return self.__width
-
-    def get_length(self):
-        return self.__length
-
-    def set_width(self, width):
-        self.__width = (width if width < 20. else 20.) if width > 0. else 0.
-
-    def set_length(self, length):
-        self.__length = (length if length < 20. else 20.) if length > 0. else 0.
+    def __setattr__(self, key, value):
+        if key != '_Rectangle__width' and key != '_Rectangle__length':
+            raise AttributeError(f'class does not have attribute "{key}"')
+        if not isinstance(value, float):
+            raise TypeError(f'type of {key} should be "float')
+        if value <= 0 or value >= 20:
+            raise ValueError("__length should be in range (0...20)")
+        self.__dict__[key] = value
 
 
-x = Rectangle(3, 26)
-print(x.square(), ' ', x.perimetr())
+x = Rectangle(3., 6.)
+print(x.square(), x.perimeter())
--- Task2.py
+++ Task2.py
@@ -1,33 +1,42 @@
-from operator import *
-
 class Rational:
+    """rational number"""
     def __init__(self, numerator=1, denominator=1):
-        # self.sign = numerator < 0 ^ denominator < 0
-        self.__numerator = int(abs(numerator)/nsd(numerator, denominator))
-        self.__denominator = int(denominator/nsd(numerator, denominator))
+        if not isinstance(numerator, int):
+            raise TypeError('type of numerator should be "int"')
+        if not isinstance(denominator, int):
+            raise TypeError('type of denominator should be "int"')
+        if not denominator:
+            raise ValueError("denominator must not be 0")
+        self.__sign = numerator*denominator < 0
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+        self.__numerator = numerator//Rational.nsd(numerator, denominator)
+        self.__denominator = denominator//Rational.nsd(numerator, denominator)
 
     def __str__(self):
-        return str(self.__numerator) + '/' + str(self.__denominator)
+        if self.__sign:
+            return '-'+str(self.__numerator) + '/' + str(self.__denominator)
+        else:
+            return str(self.__numerator) + '/' + str(self.__denominator)
 
     def __float__(self):
-        try:
-            return float(self.__numerator)/self.__denominator
-        except:
-            return 0.
+        return self.__numerator/self.__denominator*(-1. if self.__sign else 1.)
 
-def nsd(a, b):
-    if a == 0 or b == 0:
-        return 1
-    while a != b:
-        if a > b:
-            a -= b
-        else:
-            b -= a
-    return a
+    @classmethod
+    def nsd(cls, a, b):
+        """find the least common divisor of b and a"""
+        if a == 0 or b == 0:
+            return 1
+        while a != b:
+            if a > b:
+                a -= b
+            else:
+                b -= a
+        return a
 
 
 a = Rational(3, 26)
 b = Rational(6, 2)
-c = Rational(9, 0)
+c = Rational(9, -1)
 print(a, b, c)
 print(float(a), float(b), float(c))
